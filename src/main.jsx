7b599c74cebc31f0d0f853e9ad84c21c57e740b6
nirjalguragainofficial-hash <nirjalguragainofficial@gmail.com>
2026-07-13 09:47:38 -0700

    docs(main): add entry point JSDoc comment

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+/**
+ * Application entry point.
+ * Mounts the React component tree to the DOM root.
+ */
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
